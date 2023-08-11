fccd7d4c08cd3d4091be7e074767f591ac3ec061
Nozomu Hara <slipper-spells-0j@icloud.com>
2023-08-11 11:22:46 +0900

    added some style to watch-list

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :overview, presence: true
+  # validates :poster_url, presence: true
+  # validates :rating, presence: true
+end
